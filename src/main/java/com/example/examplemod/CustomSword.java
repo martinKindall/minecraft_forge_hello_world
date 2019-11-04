14ca70dea56be72b8af83d5942e4fffa4e079f8b
martinKindall <mart256@gmail.com>
2019-11-04 11:08:42 -0400

    new class CustomSword, ExampleMod creates myToolMaterial

---
+++ src/main/java/com/example/examplemod/CustomSword.java
@@ -0,0 +1,7 @@
+package com.example.examplemod;
+
+public class CustomSword extends net.minecraft.item.ItemSword {
+    public CustomSword(ToolMaterial material) {
+        super(material);
+    }
+}
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,6 +1,8 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -10,16 +12,28 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "sword";
+    public static final String NAME = "Mod de prueba";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
+
+    public static Item.ToolMaterial myToolMaterial;
+    public static Item mySword;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        myToolMaterial = EnumHelper.addToolMaterial(
+                "Material name",
+                4,
+                10000,
+                20.0F,
+                100.0F,
+                30
+        );
+        mySword = new CustomSword(myToolMaterial);
     }
 
     @EventHandler
