f44a19679f84d565d297951b251c0ef1c8ab0500
martinKindall <mart256@gmail.com>
2019-11-04 23:39:46 -0400

    FirstBlock: new mod

---
+++ src/main/java/block/FirstBlock.java
@@ -0,0 +1,14 @@
+package block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class FirstBlock extends Block {
+    public FirstBlock() {
+        super(Material.ROCK);
+        setCreativeTab(CreativeTabs.MISC);
+        setSoundType(SoundType.STONE);
+    }
+}
